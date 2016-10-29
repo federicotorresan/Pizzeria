cae58e157ab531461591372e731212559a762462
grandogiacomo <grandogiacomo@Lab06_14.itseinaudi.local>
2016-10-29 12:14:14 +0200

    lista

--- PizzeriaTorreGrando/src/Lista.java
+++ PizzeriaTorreGrando/src/Lista.java
@@ -1,4 +1,24 @@
+import java.util.ArrayList;
 
 public class Lista {
-
+	
+	ArrayList<String> pizze = new ArrayList<String>();
+	
+	public void listino(){
+		pizze.add("Margherita");
+		pizze.add("Diavola");
+		pizze.add("Prosciutto e funghi");
+		pizze.add("Quattro stagioni");
+		pizze.add("Quattro formaggi");
+		String pizza=pizze.remove(0);
+	}
+	
+	public synchronized int pizzaInLista(String pizza){
+		this.pizza=pizza;
+		notifyAll();
+		return pizza;
+	}
 }
+
+
+
